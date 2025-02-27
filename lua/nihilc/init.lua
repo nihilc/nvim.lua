dbe5f0531202bcccf5860db26e93ec4cbb0d856c
Christian Cardenas <nihilist.cc@gmail.com>
2025-02-26 20:52:59 -0500

    add lezy and onedark

--- lua/nihilc/init.lua
+++ lua/nihilc/init.lua
@@ -3,3 +3,33 @@ require("nihilc.options")
 require("nihilc.keymaps")
 require("nihilc.autocmd")
 
+-- Setup plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = "nihilc.plugins",
+  install = {
+    colorscheme = { "onedark", "habamax" }
+  },
+  checker = {
+    enabled = false,
+  },
+  rocks = {
+    enabled = false,
+    hererocks = false,
+  }
+})
