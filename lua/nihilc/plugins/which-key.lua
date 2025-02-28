2cf7ff4da1388ee1f1ba11f43a6aacdb284d5de9
Christian Cardenas <nihilist.cc@gmail.com>
2025-02-28 12:21:13 -0500

    add which-key

---
+++ lua/nihilc/plugins/which-key.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "helix",
+    spec = {
+      { "<leader>f", group = "find" },
+      { "<leader>h", group = "help" },
+      { "<leader>u", group = "ui" },
+    },
+    win = {
+      title = false
+    },
+    icons = {
+      mappings = false,
+    },
+    show_help = false,
+    show_keys = false,
+  }
+}
