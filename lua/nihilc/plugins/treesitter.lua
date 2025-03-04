37c4fd177f21a9b355b76c7feb81896545b69c4e
Christian Cardenas <nihilist.cc@gmail.com>
2025-03-03 23:10:58 -0500

    add tresitter

---
+++ lua/nihilc/plugins/treesitter.lua
@@ -0,0 +1,43 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        -- Web
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "json",
+        "jsonc",
+        -- Languages
+        "bash",
+        "python",
+        "lua",
+        "go",
+        -- Docs / Config
+        "markdown",
+        "markdown_inline",
+        "vimdoc",
+        "make",
+        "yaml",
+        "toml",
+      },
+      sync_install = false,
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-j>',
+          node_decremental = '<c-k>',
+          scope_incremental = false
+        },
+      },
+    })
+  end
+}
