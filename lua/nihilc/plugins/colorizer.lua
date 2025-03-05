1d8dee70d8d60a4490db0411e2b2fcfd7a13ed9c
Christian Cardenas <nihilist.cc@gmail.com>
2025-03-04 21:49:44 -0500

    add colorizer

---
+++ lua/nihilc/plugins/colorizer.lua
@@ -0,0 +1,17 @@
+return {
+  "catgoose/nvim-colorizer.lua",
+  opts = {
+    user_default_options = {
+      RGB = true,
+      RRGGBB = true,
+      names = false,
+      RRGGBBAA = false,
+      AARRGGBB = false,
+      rgb_fn = true,
+      hsl_fn = true,
+      tailwind = false, -- TODO: enable when configure lsp
+      mode = "virtualtext",
+      virtualtext = "■",
+    }
+  }
+}
