a145701216c80f2ae429e403c65f065612273883
Christian Cardenas <nihilist.cc@gmail.com>
2025-03-04 20:53:13 -0500

    add nvim-tree

---
+++ lua/nihilc/plugins/nvim-tree.lua
@@ -0,0 +1,96 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  keys = {
+    { "<leader>te", function() vim.cmd("NvimTreeToggle") end, desc = "Toggle Explorer" }
+  },
+  opts = {
+    disable_netrw = true,
+    hijack_netrw = true,
+    hijack_cursor = true,
+    hijack_unnamed_buffer_when_opening = false,
+    filters = {
+      dotfiles = true,
+      custom = { "node_modules", "\\.cache", ".git", "dist" },
+    },
+    view = {
+      float = {
+        enable = true,
+        open_win_config = {
+          relative = "editor",
+          border = "single",
+          width = 35,
+          height = 25,
+          row = 0,
+          col = 999,
+        }
+      },
+    },
+    git = {
+      enable = true,
+      show_on_dirs = true,
+      show_on_open_dirs = false,
+    },
+    diagnostics = {
+      enable = false,
+      show_on_dirs = false,
+      show_on_open_dirs = false,
+      debounce_delay = 500,
+      severity = {
+        min = vim.diagnostic.severity.WARN,
+        max = vim.diagnostic.severity.ERROR,
+      },
+      icons = {
+        hint = "H",
+        info = "I",
+        warning = "W",
+        error = "E",
+      },
+    },
+    actions = {
+      change_dir = {
+        enable = true,
+        global = true,
+        restrict_above_cwd = false,
+      },
+    },
+    renderer = {
+      root_folder_label = function(path)
+        return "../" .. vim.fn.fnamemodify(path, ":t")
+      end,
+      group_empty = true,
+      highlight_git = false,
+      special_files = {
+        "Makefile",
+        "README.md",
+        "readme.md",
+        "LICENSE",
+        "dockerfile",
+        "Dockerfile"
+      },
+      indent_markers = {
+        enable = true,
+      },
+      icons = {
+        git_placement = "signcolumn",
+        show = {
+          file = false,
+          folder = false,
+          folder_arrow = false,
+          git = true,
+        },
+        glyphs = {
+          git = {
+            unstaged = "U",
+            staged = "S",
+            unmerged = "M",
+            renamed = "R",
+            untracked = "?",
+            deleted = "D",
+            ignored = "I",
+          },
+        },
+      },
+    },
+  }
+}
