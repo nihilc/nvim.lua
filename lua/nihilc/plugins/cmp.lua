a2ba9f3baebb41cea2b92043f03b0aa8ebbb60f7
Christian Cardenas <nihilist.cc@gmail.com>
2025-03-07 01:42:18 -0500

    add cmp and luasnip

---
+++ lua/nihilc/plugins/cmp.lua
@@ -0,0 +1,68 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    -- snippets
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.3.0",
+      build = "make install_jsregexp",
+    },
+    "rafamadriz/friendly-snippets",
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<c-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<c-e>'] = cmp.mapping.abort(),
+        ['<c-space>'] = cmp.mapping.complete(),
+        ['<tab>'] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable(1) then luasnip.jump(1) else fallback() end
+        end, { "i", "s" }),
+        ['<s-tab>'] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable(-1) then luasnip.jump(-1) else fallback() end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+      })
+    })
+
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      }),
+    })
+  end
+}
