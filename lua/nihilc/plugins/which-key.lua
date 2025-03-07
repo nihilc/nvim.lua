5a48e75727b8bade05b91f37dd69ec1510bb80b9
Christian Cardenas <nihilist.cc@gmail.com>
2025-03-07 04:40:11 -0500

    register lsp in which-key

--- lua/nihilc/plugins/which-key.lua
+++ lua/nihilc/plugins/which-key.lua
@@ -7,6 +7,7 @@ return {
       { "<leader>f", group = "find" },
       { "<leader>g", group = "git", mode = { "n", "v" } },
       { "<leader>h", group = "help" },
+      { "<leader>l", group = "lsp" },
       { "<leader>t", group = "toggle" },
       { "<leader>u", group = "ui" },
     },
