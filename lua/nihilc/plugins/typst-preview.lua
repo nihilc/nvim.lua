07607a29d14975957cb3101e33925f20b00d2b8b
Christian Cardenas <nihilist.cc@gmail.com>
2025-05-11 00:19:07 -0500

    update typst-preview config, add keymaps

--- lua/nihilc/plugins/typst-preview.lua
+++ lua/nihilc/plugins/typst-preview.lua
@@ -2,5 +2,21 @@ return {
   "chomosuke/typst-preview.nvim",
   lazy = false,
   version = "1.*",
-  opts = {},
+  config = function()
+    local tp = require("typst-preview")
+    local keymaps = require("nihilc.keymaps")
+
+    tp.setup({
+      invert_colors = "auto",
+      dependencies_bin = {
+        ["tinymist"] = "tinymist",
+        ["websocat"] = nil,
+      },
+    })
+
+    keymaps.set({
+      { lhs = "<leader>tp", rhs = "<cmd>TypstPreviewToggle<cr>", desc = "Typst Preview" },
+      { lhs = "<leader>ts", rhs = "<cmd>TypstPreviewSyncCursor<cr>", desc = "Typst Sync" },
+    })
+  end,
 }
