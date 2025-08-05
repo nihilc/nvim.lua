62684cee07f19fa8e72c8d17ba2748df841c4806
Christian Cardenas <nihilist.cc@gmail.com>
2025-08-05 16:44:21 -0500

    add kanagawa theme

--- lua/nihilc/plugins/colorshemes.lua
+++ lua/nihilc/plugins/colorshemes.lua
@@ -1,7 +1,7 @@
 return {
   {
     "navarasu/onedark.nvim",
-    enabled = true,
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
@@ -17,6 +17,35 @@ return {
         },
       })
       require("onedark").load()
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    enabled = true,
+    lazy = false,
+    config = function()
+      require("kanagawa").setup({
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            IblIndent = { fg = colors.palette.sumiInk4 },
+            NvimTreeIndentMarker = { fg = colors.palette.sumiInk4 },
+          }
+        end,
+      })
+      vim.cmd("colorscheme kanagawa")
     end,
   },
 }
